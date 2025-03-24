5010076c8a5312abceb49a517c3a30336411f95b
vuvansy <vuvansy3001@gmail.com>
2025-03-24 14:47:34 +0700

    API get user stats, order stats & get lơ stock book

--- routes/stats.js
+++ routes/stats.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-const { 
-    getStatsAPI, getRevenueStatsAPI
- } = require('../controllers/StatsController')
+const {
+    getStatsAPI, getRevenueStatsAPI, getUserStatsAPI,
+    getOrderStatsAPI, getLowStockBooksAPI
+} = require('../controllers/StatsController')
 
 
 router.get('/stats', getStatsAPI);
 router.get("/stats/revenue", getRevenueStatsAPI);
+router.get("/stats/users", getUserStatsAPI);
+router.get("/stats/orders", getOrderStatsAPI);
+router.get("/books/low-stock", getLowStockBooksAPI);
+
 
 module.exports = router;
